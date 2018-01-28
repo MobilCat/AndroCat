f59a90c10317536c1c56a03a2db929be61acea18
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2018-01-28 23:02:05 +0300

    WebView and it's functionality implemented

--- app/src/main/java/mustafaozhan/github/com/githubclient/MainActivity.kt
+++ app/src/main/java/mustafaozhan/github/com/githubclient/MainActivity.kt
@@ -1,12 +1,81 @@
 package mustafaozhan.github.com.githubclient
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.Window
+import android.view.WindowManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val URL = "https://github.com/login"
+    }
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
         setContentView(R.layout.activity_main)
+
+        webView.webViewClient = MyWebClient()
+        webView.settings.javaScriptEnabled = true
+        webView.settings.setSupportZoom(true)
+        webView.settings.builtInZoomControls = true
+        webView.settings.displayZoomControls = false
+        webView.setBackgroundColor(Color.parseColor("#FFFFFF"))
+        webView.settings.useWideViewPort = true
+        webView.settings.loadWithOverviewMode = false
+        webView.loadUrl(URL)
+        setDesktopMode(webView, true)
+        webView.settings.textZoom = 150
+
+
+    }
+
+    @Suppress("OverridingDeprecatedMember")
+    class MyWebClient : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+            view?.loadUrl(url)
+            return true
+        }
+
+    }
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
+        return if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack()
+            true
+        } else
+            super.onKeyUp(keyCode, event)
+    }
+
+    private fun setDesktopMode(webView: WebView, enabled: Boolean) {
+        var newUserAgent: String? = webView.settings.userAgentString
+        if (enabled) {
+            try {
+                val ua = webView.settings.userAgentString
+                val androidOSString = webView.settings.userAgentString.substring(ua.indexOf("("), ua.indexOf(")") + 1)
+                newUserAgent = webView.settings.userAgentString.replace(androidOSString, "(X11; Linux x86_64)")
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+        } else {
+            newUserAgent = null
+        }
+
+        webView.settings.userAgentString = newUserAgent
+        webView.settings.useWideViewPort = enabled
+        webView.settings.loadWithOverviewMode = enabled
+        webView.reload()
     }
 }
