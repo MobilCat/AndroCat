b126d96fbdf0e0f89eec22a2d840bafe0b4750bc
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2019-02-16 20:22:28 +0100

    MainFragment reformatted

--- app/src/main/java/mustafaozhan/github/com/androcat/main/fragment/MainFragment.kt
+++ app/src/main/java/mustafaozhan/github/com/androcat/main/fragment/MainFragment.kt
@@ -6,8 +6,10 @@ import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.view.View
-import com.crashlytics.android.Crashlytics
-import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.android.synthetic.main.fragment_main.mBottomNavigationView
+import kotlinx.android.synthetic.main.fragment_main.mImgViewAndroCat
+import kotlinx.android.synthetic.main.fragment_main.mSwipeRefreshLayout
+import kotlinx.android.synthetic.main.fragment_main.webView
 import me.piruin.quickaction.ActionItem
 import me.piruin.quickaction.QuickAction
 import mustafaozhan.github.com.androcat.R
@@ -19,6 +21,7 @@ import mustafaozhan.github.com.androcat.tools.AndroCatWebViewClient
 /**
  * Created by Mustafa Ozhan on 2018-07-22.
  */
+@Suppress("TooManyFunctions", "MagicNumber")
 class MainFragment : BaseMvvmFragment<MainFragmentViewModel>() {
 
     companion object {
@@ -37,13 +40,13 @@ class MainFragment : BaseMvvmFragment<MainFragmentViewModel>() {
     private var quickActionProfile: QuickAction? = null
     private var quickActionExplorer: QuickAction? = null
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         init()
         setDash()
         initWebView()
-        setUi()
+        setListeners()
+        setActionListeners()
         if (arguments?.getBoolean(ARGS_SHOW_ON_GITHUB) == true) {
             webView.loadUrl(getString(R.string.url_project_repository))
         }
@@ -53,105 +56,64 @@ class MainFragment : BaseMvvmFragment<MainFragmentViewModel>() {
         url = getString(R.string.url_login)
         viewModel.initUsername()
         if (viewModel.userName != resources.getString(R.string.missUsername)) {
-            url = "https://github.com"
+            url = getString(R.string.url_github)
         }
 
-        context?.let {
-            quickActionExplorer = QuickAction(it, QuickAction.VERTICAL)
+        context?.let { ctx ->
+            quickActionExplorer = QuickAction(ctx, QuickAction.VERTICAL)
             quickActionExplorer?.apply {
                 setColorRes(R.color.colorGitHubDash)
                 setTextColorRes(R.color.white)
                 setEnabledDivider(false)
-                val searchItem = ActionItem(1, getString(R.string.search), R.drawable.search_icon)
-                val marketPlaceItem = ActionItem(2, getString(R.string.market_place), R.drawable.ic_shopping_cart_black_24dp)
-                val trendsItem = ActionItem(3, getString(R.string.trends), R.drawable.ic_trending_up_black_24dp)
-                val newGistItem = ActionItem(4, getString(R.string.new_gist), R.drawable.ic_code_black_24dp)
-                val newRepoItem = ActionItem(5, getString(R.string.new_repository), R.drawable.new_repo)
-                addActionItem(searchItem, marketPlaceItem, trendsItem, newGistItem, newRepoItem)
+                addActionItem(
+                    ActionItem(1, getString(R.string.search), R.drawable.search_icon),
+                    ActionItem(2, getString(R.string.market_place), R.drawable.ic_shopping_cart_black_24dp),
+                    ActionItem(3, getString(R.string.trends), R.drawable.ic_trending_up_black_24dp),
+                    ActionItem(4, getString(R.string.new_gist), R.drawable.ic_code_black_24dp),
+                    ActionItem(5, getString(R.string.new_repository), R.drawable.new_repo))
             }
 
-            quickActionProfile = QuickAction(it, QuickAction.VERTICAL)
+            quickActionProfile = QuickAction(ctx, QuickAction.VERTICAL)
             quickActionProfile?.apply {
                 setColorRes(R.color.colorGitHubDash)
                 setTextColorRes(R.color.white)
                 setEnabledDivider(false)
-                val starItem = ActionItem(1, getString(R.string.starts), R.drawable.ic_star_black_24dp)
-                val reposItem = ActionItem(2, getString(R.string.repositories), R.drawable.new_repo)
-                val gistItem = ActionItem(3, getString(R.string.gists), R.drawable.ic_code_black_24dp)
-                val notificationsItem = ActionItem(4, getString(R.string.notifications), R.drawable.notifications)
-                val applicationSettingsItem = ActionItem(5, getString(R.string.app_settings), R.drawable.ic_settings_black_24dp)
-                val userSettings = ActionItem(6, getString(R.string.user_settings), R.drawable.user_settings)
-                val logOutItem = ActionItem(7, getString(R.string.log_out), R.drawable.logout_icon)
-                val loginItem = ActionItem(8, getString(R.string.log_in), R.drawable.login_icon)
-                val userItem = ActionItem(9, getString(R.string.profile), R.drawable.user)
-                quickActionProfile!!.addActionItem(starItem, reposItem, gistItem, notificationsItem, applicationSettingsItem, userSettings, logOutItem, loginItem, userItem)
+                addActionItem(
+                    ActionItem(1, getString(R.string.starts), R.drawable.ic_star_black_24dp),
+                    ActionItem(2, getString(R.string.repositories), R.drawable.new_repo),
+                    ActionItem(3, getString(R.string.gists), R.drawable.ic_code_black_24dp),
+                    ActionItem(4, getString(R.string.notifications), R.drawable.notifications),
+                    ActionItem(5, getString(R.string.app_settings), R.drawable.ic_settings_black_24dp),
+                    ActionItem(6, getString(R.string.user_settings), R.drawable.user_settings),
+                    ActionItem(7, getString(R.string.log_out), R.drawable.logout_icon),
+                    ActionItem(8, getString(R.string.log_in), R.drawable.login_icon),
+                    ActionItem(9, getString(R.string.profile), R.drawable.user)
+                )
             }
         }
-
-
     }
 
-    private fun setUi() {
+    private fun setListeners() {
         webView.loadUrl(url)
         mSwipeRefreshLayout.setOnRefreshListener {
             webView.loadUrl(webView.url)
             mSwipeRefreshLayout.isRefreshing = false
         }
-        mBottomNavigationView.setOnNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.navigation_user -> quickActionProfile!!.show(mBottomNavigationView.getIconAt(4))
-                R.id.navigation_find -> quickActionExplorer!!.show(mBottomNavigationView.getIconAt(3))
+        mBottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_user -> quickActionProfile?.show(mBottomNavigationView.getIconAt(4))
+                R.id.navigation_find -> quickActionExplorer?.show(mBottomNavigationView.getIconAt(3))
                 R.id.navigation_feed -> {
-                    if (viewModel.userName == resources.getString(R.string.username))
+                    if (viewModel.userName == resources.getString(R.string.username)) {
                         webView.loadUrl(getString(R.string.url_login))
-                    else
+                    } else {
                         webView.loadUrl(getString(R.string.url_github))
+                    }
                 }
                 R.id.navigation_pull_request -> webView.loadUrl(getString(R.string.url_pulls))
                 R.id.navigation_Issues -> webView.loadUrl(getString(R.string.url_issues))
             }
             true
-        }
-        quickActionProfile!!.setOnActionItemClickListener {
-            when (it.actionId) {
-                1 -> if (viewModel.userName == resources.getString(R.string.missUsername))
-                    getBaseActivity().snacky("Please enter your username", true, "Enter", true)
-                else
-                    webView.loadUrl("https://github.com/" + viewModel.userName + "?tab=stars")
-
-                2 -> if (viewModel.userName == resources.getString(R.string.missUsername))
-                    getBaseActivity().snacky("Please enter your username", true, "Enter", true)
-                else
-                    webView.loadUrl("https://github.com/" + viewModel.userName + "?tab=repositories")
-
-                3 -> if (viewModel.userName == resources.getString(R.string.missUsername))
-                    getBaseActivity().snacky("Please enter your username", true, "Enter", true)
-                else
-                    webView.loadUrl("https://gist.github.com/" + viewModel.userName)
-
-                4 -> webView.loadUrl("https://github.com/notifications")
-                5 -> replaceFragment(SettingsFragment.newInstance(), true)
-                6 -> webView.loadUrl("https://github.com/settings")
-                7 -> webView.loadUrl("https://github.com/logout")
-                8 -> webView.loadUrl("https://gist.github.com/login")
-                9 -> if (viewModel.userName == resources.getString(R.string.missUsername))
-                    getBaseActivity().snacky("Please enter your username", true, "Enter", true)
-                else
-                    webView.loadUrl("https://github.com/" + viewModel.userName)
-
-
-            }
-
-        }
-        quickActionExplorer!!.setOnActionItemClickListener {
-            when (it.actionId) {
-                1 -> webView.loadUrl("https://github.com/search")
-                2 -> webView.loadUrl("https://github.com/marketplace")
-                3 -> webView.loadUrl("https://github.com/trending")
-                4 -> webView.loadUrl("https://gist.github.com/")
-                5 -> webView.loadUrl("https://github.com/new")
-                else -> webView.loadUrl("https://github.com/")
-            }
         }
 
         mImgViewAndroCat.apply {
@@ -163,7 +125,40 @@ class MainFragment : BaseMvvmFragment<MainFragmentViewModel>() {
             setArchStroke(24f)
             setArchSpeed(12)
         }
+    }
 
+    private fun loadIfUserNameSet(url: String) {
+        if (viewModel.userName == resources.getString(R.string.missUsername))
+            getBaseActivity().snacky(getString(R.string.missUsername), true, getString(R.string.enter), true)
+        else
+            webView.loadUrl(url)
+    }
+
+    @Suppress("ComplexMethod")
+    private fun setActionListeners() {
+        quickActionProfile?.setOnActionItemClickListener { item ->
+            when (item.actionId) {
+                1 -> loadIfUserNameSet(getString(R.string.url_github) + viewModel.userName + "?tab=stars")
+                2 -> loadIfUserNameSet(getString(R.string.url_github) + viewModel.userName + "?tab=repositories")
+                3 -> loadIfUserNameSet(getString(R.string.url_gist) + viewModel.userName)
+                4 -> webView.loadUrl(getString(R.string.url_notifications))
+                5 -> replaceFragment(SettingsFragment.newInstance(), true)
+                6 -> webView.loadUrl(getString(R.string.url_settings))
+                7 -> webView.loadUrl(getString(R.string.url_logout))
+                8 -> webView.loadUrl(getString(R.string.url_gist_login))
+                9 -> loadIfUserNameSet(getString(R.string.url_github) + viewModel.userName)
+            }
+        }
+        quickActionExplorer?.setOnActionItemClickListener { item ->
+            when (item.actionId) {
+                1 -> webView.loadUrl(getString(R.string.url_search))
+                2 -> webView.loadUrl(getString(R.string.url_market_place))
+                3 -> webView.loadUrl(getString(R.string.url_trending))
+                4 -> webView.loadUrl(getString(R.string.url_gist))
+                5 -> webView.loadUrl(getString(R.string.url_new))
+                else -> webView.loadUrl(getString(R.string.url_github))
+            }
+        }
     }
 
     private fun setDash() {
@@ -179,26 +174,24 @@ class MainFragment : BaseMvvmFragment<MainFragmentViewModel>() {
 
     @SuppressLint("SetJavaScriptEnabled")
     private fun initWebView() {
-        webView.webViewClient = AndroCatWebViewClient(mImgViewAndroCat)
-        var newUserAgent: String? = webView.settings.userAgentString
-        try {
-            val ua = webView.settings.userAgentString
-            val androidOSString = webView.settings.userAgentString.substring(ua.indexOf("("), ua.indexOf(")") + 1)
-            newUserAgent = webView.settings.userAgentString.replace(androidOSString, "(X11; Linux x86_64)")
-        } catch (e: Exception) {
-            Crashlytics.logException(e)
-            e.printStackTrace()
-        }
-        webView.settings.userAgentString = newUserAgent
-        webView.settings.useWideViewPort = true
-        webView.settings.loadWithOverviewMode = true
-        webView.settings.javaScriptEnabled = true
-        webView.settings.setSupportZoom(true)
-        webView.settings.builtInZoomControls = true
-        webView.settings.displayZoomControls = false
-        webView.setBackgroundColor(Color.parseColor("#FFFFFF"))
-    }
+        webView.apply {
+            webViewClient = AndroCatWebViewClient(mImgViewAndroCat)
+            setBackgroundColor(Color.parseColor("#FFFFFF"))
 
+            settings.apply {
+                val ua = userAgentString
+                val androidOSString = userAgentString.substring(ua.indexOf("("), ua.indexOf(")") + 1)
+
+                userAgentString = userAgentString.replace(androidOSString, "(X11; Linux x86_64)")
+                useWideViewPort = true
+                loadWithOverviewMode = true
+                javaScriptEnabled = true
+                setSupportZoom(true)
+                builtInZoomControls = true
+                displayZoomControls = false
+            }
+        }
+    }
 
     override fun getViewModelClass(): Class<MainFragmentViewModel> = MainFragmentViewModel::class.java
 
@@ -211,9 +204,7 @@ class MainFragment : BaseMvvmFragment<MainFragmentViewModel>() {
             webView.loadUrl(MainActivity.uri)
             MainActivity.uri = null
         }
-
     }
-
 
     override fun onDestroy() {
         super.onDestroy()
