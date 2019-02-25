0ab4bc35abe2d06affe2ab617d42acf8012d8db9
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2019-02-25 22:20:10 +0100

    Base Api helper added

---
+++ app/src/main/java/mustafaozhan/github/com/androcat/base/api/BaseApiHelper.kt
@@ -0,0 +1,29 @@
+package mustafaozhan.github.com.androcat.base.api
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import mustafaozhan.github.com.androcat.application.Application
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * Created by Mustafa Ozhan on 7/10/18 at 9:44 PM on Arch Linux wit Love <3.
+ */
+abstract class BaseApiHelper {
+
+    protected val gson: Gson
+        get() = GsonBuilder().create()
+
+    protected fun getString(resId: Int): String {
+        return Application.instance.getString(resId)
+    }
+
+    protected fun initRxRetrofit(endpoint: String, httpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(endpoint)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(httpClient)
+            .build()
+    }
+}
