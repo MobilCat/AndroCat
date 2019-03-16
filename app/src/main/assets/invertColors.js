153db1ee7c5dafc9b50c63e7147c068ddfb18799
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2019-03-16 21:43:09 +0100

    invertColors.js Updated

--- app/src/main/assets/invertColors.js
+++ app/src/main/assets/invertColors.js
@@ -1,13 +1,19 @@
 javascript: (
     function() {
-        var css = 'html {-webkit-filter: invert(100%);' +
-            '-moz-filter: invert(100%);' +
-            '-o-filter: invert(100%);' +
-            '-ms-filter: invert(100%); }';
+        var css = 'html {-webkit-filter: grayscale(50%) invert(100%);' +
+            '-moz-filter: grayscale(50%) invert(100%);' +
+            '-o-filter: grayscale(50%) invert(100%);' +
+            '-ms-filter: grayscale(50%) invert(100%); }'+
+            'img {-webkit-filter:invert(100%);'+
+                '}';
+
         var white = '#ffffff';
         var darkDash = '#1e2326';
-        var darkDashInverted = '#e1dcd9'
         var dash = '#24292f';
+        var lightBlue='#f6f8fa'
+
+        var darkDashInverted = '#e1dcd9'
+        var lightBlueInverted = '#090705'
 
         head = document.getElementsByTagName('head')[0];
         body = document.getElementsByTagName('body')[0];
@@ -43,7 +49,8 @@ javascript: (
         }
 
         function getNormalColors() {
-            var css = 'html {-webkit-filter: invert(0%); -moz-filter:    invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }'
+            var css = 'html {-webkit-filter: invert(0%); -moz-filter:    invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }'+
+            'img {-webkit-filter:invert(0%);}'
             style.type = 'text/css';
             if (style.styleSheet) {
                 style.styleSheet.cssText = css;
@@ -55,7 +62,7 @@ javascript: (
             body.appendChild(style);
             body.style.backgroundColor = white;
             document.getElementsByClassName('dashboard-sidebar js-sticky top-0 px-3 px-md-4 px-lg-4 overflow-auto')[0].style.backgroundColor = white;
-            document.getElementsByClassName('col-12 col-md-8 col-lg-6 p-responsive mt-3 border-bottom')[0].style.backgroundColor = white;
-            document.getElementsByClassName('team-left-column col-12 col-md-3 col-lg-3 pr-5 mt-5 hide-lg hide-md hide-sm border-bottom')[0].style.backgroundColor = white;
+            document.getElementsByClassName('col-12 col-md-8 col-lg-6 p-responsive mt-3 border-bottom')[0].style.backgroundColor = lightBlue;
+            document.getElementsByClassName('team-left-column col-12 col-md-3 col-lg-3 pr-5 mt-5 hide-lg hide-md hide-sm border-bottom')[0].style.backgroundColor = lightBlue;
         }
     })()
