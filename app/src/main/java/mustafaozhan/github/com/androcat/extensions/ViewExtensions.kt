cf3b2e3f403f76ab4238d59ba61621f4d9ea09b8
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2019-04-29 21:31:43 +0200

    Logging exception

--- app/src/main/java/mustafaozhan/github/com/androcat/extensions/ViewExtensions.kt
+++ app/src/main/java/mustafaozhan/github/com/androcat/extensions/ViewExtensions.kt
@@ -1,5 +1,7 @@
 package mustafaozhan.github.com.androcat.extensions
 
+import android.util.Log
+import com.crashlytics.android.Crashlytics
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
@@ -10,16 +12,24 @@ import mustafaozhan.github.com.androcat.tools.JsScrip
  * Created by Mustafa Ozhan on 1/30/18 at 12:42 AM on Arch Linux wit Love <3.
  */
 fun AdvancedWebView.runScript(jsScrip: JsScrip, action: (String) -> Unit = {}) =
-    evaluateJavascript(
-        context
-            .assets
-            .open(jsScrip.value)
-            .bufferedReader()
-            .use {
-                it.readText()
-            },
-        action
-    )
+    try {
+        evaluateJavascript(
+            context
+                .assets
+                .open(jsScrip.value)
+                .bufferedReader()
+                .use {
+                    it.readText()
+                },
+            action
+        )
+    } catch (exception: NoSuchMethodError) {
+        Crashlytics.logException(exception)
+        Crashlytics.log(Log.ERROR,
+            "NoSuchMethodError at AdvancedWebView.runScript",
+            "jsScript:${jsScrip.value}"
+        )
+    }
 
 fun AdView.loadAd(adId: Int) {
     MobileAds.initialize(context, resources.getString(adId))
