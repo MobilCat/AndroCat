c9de83085dcc8dba3b4cbf4bfec18edb5566aabf
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2019-05-04 22:53:56 +0200

    Design Updates

--- app/src/main/java/mustafaozhan/github/com/androcat/view/CurvedBottomNavigationView.kt
+++ app/src/main/java/mustafaozhan/github/com/androcat/view/CurvedBottomNavigationView.kt
@@ -12,9 +12,7 @@ import mustafaozhan.github.com.androcat.R
 
 @Suppress("MagicNumber")
 class CurvedBottomNavigationView : BottomNavigationViewEx {
-    /**
-     * the CURVE_CIRCLE_RADIUS represent the radius of the fab button
-     */
+
     companion object {
         const val CURVE_CIRCLE_RADIUS = 88
     }
@@ -22,13 +20,11 @@ class CurvedBottomNavigationView : BottomNavigationViewEx {
     private var mPath = Path()
     private var mPaint = Paint()
 
-    // the coordinates of the first curve
     private val mFirstCurveStartPoint = Point()
     private val mFirstCurveEndPoint = Point()
     private val mFirstCurveControlPoint1 = Point()
     private val mFirstCurveControlPoint2 = Point()
 
-    // the coordinates of the second curve
     private var mSecondCurveStartPoint = Point()
     private val mSecondCurveEndPoint = Point()
     private val mSecondCurveControlPoint1 = Point()
@@ -55,41 +51,59 @@ class CurvedBottomNavigationView : BottomNavigationViewEx {
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        // get width and height of navigation bar
-        // Navigation bar bounds (width & height)
 
-        // the coordinates (x,y) of the start point before curve
         mFirstCurveStartPoint.set(width / 2 - CURVE_CIRCLE_RADIUS * 2 - CURVE_CIRCLE_RADIUS / 8, 0)
-        // the coordinates (x,y) of the end point after curve
+
         mFirstCurveEndPoint.set(width / 2, CURVE_CIRCLE_RADIUS + CURVE_CIRCLE_RADIUS / 8)
-        // same thing for the second curve
+
         mSecondCurveStartPoint = mFirstCurveEndPoint
         mSecondCurveEndPoint.set(width / 2 + CURVE_CIRCLE_RADIUS * 2 + CURVE_CIRCLE_RADIUS / 8, 0)
 
-        // the coordinates (x,y)  of the 1st control point on a cubic curve
-        mFirstCurveControlPoint1
-            .set(mFirstCurveStartPoint.x + CURVE_CIRCLE_RADIUS + CURVE_CIRCLE_RADIUS / 32, mFirstCurveStartPoint.y)
-        // the coordinates (x,y)  of the 2nd control point on a cubic curve
-        mFirstCurveControlPoint2
-            .set(mFirstCurveEndPoint.x - CURVE_CIRCLE_RADIUS * 2 + CURVE_CIRCLE_RADIUS, mFirstCurveEndPoint.y)
+        mFirstCurveControlPoint1.set(
+            mFirstCurveStartPoint.x + CURVE_CIRCLE_RADIUS + CURVE_CIRCLE_RADIUS / 32,
+            mFirstCurveStartPoint.y
+        )
 
-        mSecondCurveControlPoint1
-            .set(mSecondCurveStartPoint.x + CURVE_CIRCLE_RADIUS * 2 - CURVE_CIRCLE_RADIUS, mSecondCurveStartPoint.y)
-        mSecondCurveControlPoint2
-            .set(mSecondCurveEndPoint.x - (CURVE_CIRCLE_RADIUS + CURVE_CIRCLE_RADIUS / 32), mSecondCurveEndPoint.y)
+        mFirstCurveControlPoint2.set(
+            mFirstCurveEndPoint.x - CURVE_CIRCLE_RADIUS * 2 + CURVE_CIRCLE_RADIUS,
+            mFirstCurveEndPoint.y
+        )
+
+        mSecondCurveControlPoint1.set(
+            mSecondCurveStartPoint.x + CURVE_CIRCLE_RADIUS * 2 - CURVE_CIRCLE_RADIUS,
+            mSecondCurveStartPoint.y
+        )
+
+        mSecondCurveControlPoint2.set(
+            mSecondCurveEndPoint.x - (CURVE_CIRCLE_RADIUS + CURVE_CIRCLE_RADIUS / 32),
+            mSecondCurveEndPoint.y
+        )
 
         mPath.apply {
             reset()
             moveTo(0f, 0f)
-            lineTo(mFirstCurveStartPoint.x.toFloat(), mFirstCurveStartPoint.y.toFloat())
+            lineTo(
+                mFirstCurveStartPoint.x.toFloat(),
+                mFirstCurveStartPoint.y.toFloat()
+            )
 
-            cubicTo(mFirstCurveControlPoint1.x.toFloat(), mFirstCurveControlPoint1.y.toFloat(),
-                mFirstCurveControlPoint2.x.toFloat(), mFirstCurveControlPoint2.y.toFloat(),
-                mFirstCurveEndPoint.x.toFloat(), mFirstCurveEndPoint.y.toFloat())
+            cubicTo(
+                mFirstCurveControlPoint1.x.toFloat(),
+                mFirstCurveControlPoint1.y.toFloat(),
+                mFirstCurveControlPoint2.x.toFloat(),
+                mFirstCurveControlPoint2.y.toFloat(),
+                mFirstCurveEndPoint.x.toFloat(),
+                mFirstCurveEndPoint.y.toFloat()
+            )
 
-            cubicTo(mSecondCurveControlPoint1.x.toFloat(), mSecondCurveControlPoint1.y.toFloat(),
-                mSecondCurveControlPoint2.x.toFloat(), mSecondCurveControlPoint2.y.toFloat(),
-                mSecondCurveEndPoint.x.toFloat(), mSecondCurveEndPoint.y.toFloat())
+            cubicTo(
+                mSecondCurveControlPoint1.x.toFloat(),
+                mSecondCurveControlPoint1.y.toFloat(),
+                mSecondCurveControlPoint2.x.toFloat(),
+                mSecondCurveControlPoint2.y.toFloat(),
+                mSecondCurveEndPoint.x.toFloat(),
+                mSecondCurveEndPoint.y.toFloat()
+            )
 
             lineTo(width.toFloat(), 0f)
             lineTo(width.toFloat(), height.toFloat())
