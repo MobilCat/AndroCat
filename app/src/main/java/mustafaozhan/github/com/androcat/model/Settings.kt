35fa19de424ef9dac820eb00effa6dc0040a9ec9
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2019-05-12 14:21:48 +0200

    Bug Fix

--- app/src/main/java/mustafaozhan/github/com/androcat/model/Settings.kt
+++ app/src/main/java/mustafaozhan/github/com/androcat/model/Settings.kt
@@ -2,5 +2,5 @@ package mustafaozhan.github.com.androcat.model
 
 data class Settings(
     var isInvert: Boolean? = false,
-    var isFirstTime: Boolean? = false
+    var isFirstTime: Boolean? = true
 )
