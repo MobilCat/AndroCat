4604f12ca4084502045cf8df1a12ec41a06c2af6
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2019-07-13 21:05:03 +0200

    Bug Fix #33,#34,#22

--- app/src/main/java/mustafaozhan/github/com/androcat/main/fragment/MainFragment.kt
+++ app/src/main/java/mustafaozhan/github/com/androcat/main/fragment/MainFragment.kt
@@ -393,7 +393,10 @@ class MainFragment : BaseMvvmFragment<MainFragmentViewModel>(), AdvancedWebView.
                 when (item.actionId) {
                     1 -> loadIfUserNameSet(getString(R.string.url_github) + viewModel.getUserName())
                     2 -> loadUrlWithAnimation(getString(R.string.url_login))
-                    3 -> loadIfUserNameSet(getString(R.string.url_logout))
+                    3 -> {
+                        logoutCount = 0
+                        loadIfUserNameSet(getString(R.string.url_logout))
+                    }
                     4 -> loadIfUserNameSet(getString(R.string.url_settings))
                     5 -> replaceFragment(SettingsFragment.newInstance(), true)
                 }
