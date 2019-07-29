cb22dda25696be06db355b18dae91a465eda0900
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2019-07-29 20:45:00 +0200

    Slider is done

--- app/src/main/java/mustafaozhan/github/com/androcat/splash/SplashActivityViewModel.kt
+++ app/src/main/java/mustafaozhan/github/com/androcat/splash/SplashActivityViewModel.kt
@@ -3,7 +3,5 @@ package mustafaozhan.github.com.androcat.splash
 import mustafaozhan.github.com.androcat.base.BaseViewModel
 
 class SplashActivityViewModel : BaseViewModel() {
-    override fun inject() {
-        viewModelComponent.inject(this)
-    }
+    override fun inject() = viewModelComponent.inject(this)
 }
