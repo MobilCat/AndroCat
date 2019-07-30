69e2c3f88fb294cf0a987a476ae40bcffb905ee1
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2019-07-30 20:03:45 +0200

    Retrofit Exception Handling  converted to Kotlin

---
+++ app/src/main/java/mustafaozhan/github/com/androcat/base/api/RetrofitException.kt
@@ -0,0 +1,54 @@
+package mustafaozhan.github.com.androcat.base.api
+
+import mustafaozhan.github.com.androcat.R
+import retrofit2.Response
+import retrofit2.Retrofit
+import java.io.IOException
+
+/**
+ * Created by Mustafa Ozhan on 7/10/18 at 9:45 PM on Arch Linux wit Love <3.
+ */
+class RetrofitException(
+    message: String?,
+    val url: String?,
+    private val response: Response<*>?,
+    val kind: Kind, exception: Throwable?,
+    private val retrofit: Retrofit?) : RuntimeException(message, exception
+) {
+    companion object {
+
+        fun httpError(url: String, response: Response<*>, retrofit: Retrofit): RetrofitException {
+            val message = response.code().toString() + " " + response.message()
+            return RetrofitException(message, url, response, Kind.HTTP, null, retrofit)
+        }
+
+        fun networkError(exception: IOException): RetrofitException {
+            return RetrofitException(exception.message, null, null, Kind.NETWORK, exception, null)
+        }
+
+        fun unexpectedError(exception: Throwable): RetrofitException {
+            return RetrofitException(exception.message, null, null, Kind.UNEXPECTED, exception, null)
+        }
+    }
+
+    /**
+     * HTTP mResponse body converted to specified `type`. `null` if there is no
+     * mResponse.
+     *
+     * @throws IOException if unable to convert the body to the specified `type`.
+     */
+    @Throws(IOException::class)
+    fun <T> getErrorBodyAs(type: Class<T>): T? {
+        val converter = retrofit?.responseBodyConverter<T>(type, arrayOfNulls(0))
+        return response?.errorBody()?.let { converter?.convert(it) }
+    }
+
+    /**
+     * Identifies the event mKind which triggered a [RetrofitException].
+     */
+    enum class Kind(val errorMessage: Int?) {
+        NETWORK(R.string.network_error_connection),
+        HTTP(R.string.network_error_server),
+        UNEXPECTED(R.string.network_error_unexpected)
+    }
+}
